dd9a654582c3c223a0533d1672aa4b227ba4bc71
Onur Neşvat <onurnesvat@gmail.com>
2017-06-06 16:02:52 +0300

    Add incldues

--- webhook.php
+++ webhook.php
@@ -7,6 +7,10 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
+require  __DIR__ . "/src/StartCommand.php";
+require  __DIR__ . "/src/AuthCommand.php";
+
+
 $telegram = new Api($_ENV['TELEGRAM_KEY']);
 
 $telegram->addCommands([
