88d1081a9d23647cf7b4189eebed9bc5c26dce0f
Onur Neşvat <onurnesvat@gmail.com>
2017-06-06 16:31:41 +0300

    Add GradesCommand

--- run.php
+++ run.php
@@ -63,7 +63,7 @@ foreach ($database as $telegram_id => $user) {
         $telegram = new Api($_ENV['TELEGRAM_KEY']);
         $telegram->sendMessage(['chat_id' => $user['telegram_id'], 'text' => "<b>Grade Changed!!!</b>\n" . "<pre>" . $message . "</pre>", 'parse_mode' => 'HTML']);
 
-        $users[$telegram_id]['last_hash'] = md5($output);
+        $database[$telegram_id]['last_hash'] = md5($output);
     }
 
 
