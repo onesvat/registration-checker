ffe2923e1b6448316c7d3fbf803167969dbd90d0
Onur Neşvat <onurnesvat@gmail.com>
2017-06-06 16:48:45 +0300

    Add webhook setter

---
+++ webhook-set.php
@@ -0,0 +1,11 @@
+<?php
+
+use Telegram\Bot\Api;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$telegram = new Api($_ENV['TELEGRAM_KEY']);
+$telegram->setWebhook(['url' => '']);
