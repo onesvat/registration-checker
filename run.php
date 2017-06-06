bb2792abbcea2347ce311599c00a5fe3c5fba16a
Onur Neşvat <onurnesvat@gmail.com>
2017-06-06 16:26:32 +0300

    Add GradesCommand

--- run.php
+++ run.php
@@ -1,5 +1,6 @@
 <?php
 
+use Sunra\PhpSimple\HtmlDomParser;
 use Telegram\Bot\Api;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -7,41 +8,61 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$cookie_location = tempnam("/tmp", "registration-");
-$last_hash_location = $_ENV['LOC_HASH'];
-$user_name = $_ENV['USERNAME'];
-$user_pass = $_ENV['PASSWORD'];
+$database_str = file_get_contents($_ENV['DATABASE_LOC']);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://registration.boun.edu.tr/scripts/stuinflogin.asp");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-    "user_name=$user_name&user_pass=$user_pass");
+if ($database_str)
+    $database = json_decode(file_get_contents($_ENV['DATABASE_LOC']), true);
+else
+    $database = [];
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+foreach ($database as $user) {
 
-curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_location);
-curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_location);
+    $cookie_location = tempnam("/tmp", "registration-");
 
-curl_exec($ch);
-curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://registration.boun.edu.tr/scripts/stuinflogin.asp");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+        "user_name={$user['username']}&user_pass={$user['password']}");
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_location);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_location);
+
+    curl_exec($ch);
+    curl_close($ch);
 
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://registration.boun.edu.tr/scripts/stuinfgs.asp?donem=2016/2017-2");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://registration.boun.edu.tr/scripts/stuinfgs.asp?donem=2016/2017-2");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_location);
-curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_location);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_location);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_location);
 
-$output = curl_exec($ch);
-curl_close($ch);
+    $output = curl_exec($ch);
+    curl_close($ch);
 
-if (md5($output) != file_get_contents($last_hash_location)) {
-    file_put_contents($last_hash_location, md5($output));
+    if (md5($output) != $user['last_hash']) {
+        $dom = HtmlDomParser::str_get_html($output);
 
-    return new Api($_ENV['TELEGRAM_API_KEY']);
+        $message = "";
+
+        foreach ($dom->find('table', 1)->find("tr[class=recmenu]") as $element) {
+            $course = str_replace("&nbsp;", "", $element->find("td", 0)->plaintext);
+            $grade = str_replace("&nbsp;", "", $element->find("td", 3)->plaintext);
+
+            if ($grade == "") {
+                $grade = "NA";
+            }
+
+            $message .= $course . " " . $grade . "\n";
+        }
+
+        $telegram = new Api($_ENV['TELEGRAM_KEY']);
+        $telegram->sendMessage(['chat_id' => $user['telegram_id'], 'text' => "<b>Grade Changed!!!</b>\n" . "<pre>" . $message . "</pre>", 'parse_mode' => 'HTML']);
+    }
 }
 
-
-echo $output;
+file_put_contents($_ENV['DATABASE_LOC'], json_encode($database));
