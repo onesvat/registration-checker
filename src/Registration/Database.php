1d2dcc762380f1bb32a48fb92a663d985fc7580f
Onur Neşvat <onurnesvat@gmail.com>
2017-06-06 22:56:29 +0300

    V0.1
    - Add mysql database
    - Interact with registration.boun.edu.tr via Classes

--- src/Registration/Database.php
+++ src/Registration/Database.php
@@ -13,6 +13,8 @@ class Database
 {
     public static function getPdo()
     {
-        return new \PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_DATABASE'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        $pdo = new \PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_DATABASE'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $pdo;
     }
 }
