d0365777102c988721ef9a2d998e6442925c6b85
Onur Neşvat <onurnesvat@gmail.com>
2018-01-04 13:04:31 +0300

    Report only changed courses

--- run.php
+++ run.php
@@ -26,16 +26,17 @@ foreach ($users as $user) {
     }
 
     $hash = md5(json_encode($grades));
+    $grades_json = json_encode($grades['courses']);
 
     $stmt = $pdo->prepare("UPDATE users SET updated_at = ? WHERE telegram_id = ?");
     $stmt->execute([date("Y-m-d H:i:s"), $user['telegram_id']]);
 
     if ($user['last_term'] != $term) {
-        $stmt = $pdo->prepare("UPDATE users SET last_term = ?, last_hash = null WHERE telegram_id = ?");
+        $stmt = $pdo->prepare("UPDATE users SET last_term = ?, last_hash = null, last_grades = null WHERE telegram_id = ?");
         $stmt->execute([$term, $user['telegram_id']]);
 
         try {
-            $telegram->sendMessage(['chat_id' => $user['telegram_id'], 'text' => "Welcome to new term: <b>". $term ."</b>\n" . "Grade changes will be detected automatically.", 'parse_mode' => 'HTML']);
+            $telegram->sendMessage(['chat_id' => $user['telegram_id'], 'text' => "Welcome to new term: <b>" . $term . "</b>\n" . "Grade changes will be detected automatically.", 'parse_mode' => 'HTML']);
         } catch (\Exception $e) {
             continue;
         }
@@ -43,26 +44,28 @@ foreach ($users as $user) {
         continue;
     }
 
-    if ($user['last_hash'] == null) {
-        $pdo->exec("UPDATE users SET last_hash = '$hash' WHERE id = {$user['id']}");
+    if ($user['last_hash'] == null || $user['last_grades'] == null) {
+        $pdo->exec("UPDATE users SET last_hash = '$hash', last_grades = '$grades_json' WHERE id = {$user['id']}");
     } else {
         if ($hash != $user['last_hash']) {
+
+            $pdo->exec("UPDATE users SET last_hash = '$hash', last_grades = '$grades_json' WHERE id = {$user['id']}");
+
             $message = "";
 
+            $last_grades = json_decode($user['last_grades'], true);
+
             foreach ($grades['courses'] as $course => $grade) {
-                $message .= $course . " " . $grade . "\n";
+                if (array_key_exists($course, $last_grades) && $last_grades[$course] != $grade) {
+                    $message .= $course . " <b>" . $grade . "</b>\n";
+                }
             }
 
-            $message .= "\nSPA: " . $grades['spa'] . "\n";
-            $message .= "GPA: " . $grades['gpa'];
-
             try {
-                $telegram->sendMessage(['chat_id' => $user['telegram_id'], 'text' => "<b>Grade Changed!!!</b>\n" . "<pre>" . $message . "</pre>", 'parse_mode' => 'HTML']);
+                $telegram->sendMessage(['chat_id' => $user['telegram_id'], 'text' => "<b>Grade Changed!!!</b>\n\n" . "<pre>" . $message . "</pre>To check all grades please send /grades command", 'parse_mode' => 'HTML']);
             } catch (\Exception $e) {
                 continue;
             }
-
-            $pdo->exec("UPDATE users SET last_hash = '$hash' WHERE id = {$user['id']}");
         }
     }
 
